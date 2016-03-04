e731000a07bf494833f08ac0a1eb8d128fcb2510
Jaroslaw Rodak <jaroslaw.rodak@humanoitgroup.com>
2016-03-04 16:52:20 +0100

    added missing files

---
+++ hg_server/attributes/mysql.rb
@@ -0,0 +1,10 @@
+default["hg_server"] = {}
+default["hg_server"]["mysql"] = {}
+
+default["hg_server"]["mysql"]["version"] = "5.6"
+default["hg_server"]["mysql"]["port"] = "3306"
+default["hg_server"]["mysql"]["server_root_password"] = "change_me!"
+default["hg_server"]["mysql"]["actions"] = [:create, :start]
+default["hg_server"]["mysql"]["service_name"] = 'default'
+
+default["mysql"] = node["hg_server"]["mysql"]
