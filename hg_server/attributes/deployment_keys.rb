43d6c6bdbd3cd471d2fec21e70fbd027c4dec3af
Konrad <konrad.narewski@humanoitgroup.com>
2016-07-19 16:41:50 +0200

    Allow ommiting deployment keys entry in data bag

--- hg_server/attributes/deployment_keys.rb
+++ hg_server/attributes/deployment_keys.rb
@@ -1,5 +1,6 @@
 DECODED_DATA_BAG = Chef::EncryptedDataBagItem.load(node["data_bag_name"], "secrets")
+deployment_keys_secrets = DECODED_DATA_BAG["deployment_keys"] || {}
 
 default["deployment_keys"] = {}
-default["deployment_keys"]["private"] = DECODED_DATA_BAG["deployment_keys"]["private"]
-default["deployment_keys"]["public"] = DECODED_DATA_BAG["deployment_keys"]["public"]
+default["deployment_keys"]["private"] = deployment_keys_secrets["private"]
+default["deployment_keys"]["public"] = deployment_keys_secrets["public"]
